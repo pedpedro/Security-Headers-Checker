1f9a3eb47e6bc18e7c8a90751a1d4d5521338a15
pedro <phneveslima@hotmail.com>
2023-03-08 15:36:29 -0300

    Use case changes - 6

--- sec-headers-check.py
+++ sec-headers-check.py
@@ -1,6 +1,3 @@
-#!/usr/bin/python3
-# -*- coding: utf-8 -*-
-
 import json
 import re
 import requests
@@ -15,20 +12,60 @@ from huepy import bold, info, run, que, bad, good, red
 requests.packages.urllib3.disable_warnings()
 
 
+ # Checa se a url é valida.
 def valid_url(url: str) -> bool:
-    """Check that the URL is well formatted."""
     parsed_url = urlparse(url)
     if not (checkers.is_url(parsed_url.geturl()) or checkers.is_ip_address(parsed_url.geturl())):
-        # prepend https if missing
+        # Adiciona https caso estiver faltando.
         parsed_url = parsed_url._replace(**{"scheme": "https"})
-        parsed_url = parsed_url._replace(**{"netloc": parsed_url[2]})  # move path to netloc
+        parsed_url = parsed_url._replace(**{"netloc": parsed_url[2]}) 
         parsed_url = parsed_url._replace(**{"path": ""})
-        # check again with fixed url
+        # Checa com a url já configurada.
         if not (checkers.is_url(parsed_url.geturl()) or checkers.is_ip_address(parsed_url.geturl())):
             return False
     return True
 
+# Função para validar as configurações do Header
+def validaHeader(nome_header, array_headers):
+    if nome_header == 'Strict-Transport-Security':
+        hsts = array_headers['Strict-Transport-Security']
+        if 'max-age=31536000' in hsts:
+            print(" ↳",good('Strict-Transport-Security configurado corretamente.'))
+        else:
+            print(" ↳",bad('Strict-Transport-Security não configurado corretamente.'))
 
+    # X-Frame-Options
+    if nome_header == 'X-Frame-Options':
+        xfo = array_headers['X-Frame-Options']
+        if xfo == 'DENY' or xfo == 'SAMEORIGIN':
+            print(" ↳",good('X-Frame-Options configurado corretamente.'))
+        else:
+            print(" ↳",bad('X-Frame-Options não configurado corretamente.'))
+
+    # X-XSS-Protection
+    if nome_header == 'X-XSS-Protection':
+        xss = array_headers['X-XSS-Protection']
+        if xss == '1; mode=block' or xss == '1':
+            print(" ↳",good('X-XSS-Protection configurado corretamente.'))
+        else:
+            print(" ↳",bad('X-XSS-Protection não configurado corretamente.'))
+
+    # X-Content-Type-Options
+    if nome_header == 'X-Content-Type-Options':
+        xcto = array_headers['X-Content-Type-Options']
+        if xcto == 'nosniff':
+            print(" ↳",good('X-Content-Type-Options configurado corretamente.'))
+        else:
+            print(" ↳",bad('X-Content-Type-Options não configurado corretamente.'))
+
+    # Referrer-Policy
+    if nome_header == 'Referrer-Policy':
+        rp = array_headers['Referrer-Policy']
+        if rp == 'no-referrer' or rp == 'no-referrer-when-downgrade' or rp == 'same-origin' or rp == 'strict-origin' or rp == 'strict-origin-when-cross-origin' or rp == 'origin' or rp == 'origin-when-cross-origin' or rp == 'unsafe-url':
+            print(" ↳",good('Referrer-Policy configurado corretamente.'))
+        else:
+            print(" ↳",bad('Referrer-Policy não configurado corretamente.'))
+    
 def main():
 
     parser = argparse.ArgumentParser()
@@ -44,41 +81,54 @@ def main():
         'Connection': 'close'
     })
 
-    # prepend https if missing
+    # Adiciona https caso não tenha.
     args.url = 'https://' + args.url if not args.url.startswith('https') else args.url
     if not valid_url(args.url):
         parser.print_help()
         exit()
+
+    try:
+        response = session.get(url=args.url)
+    except requests.exceptions.ConnectionError as e:
+        print(bold(bad(f"{bold(red('connection error'))}: {e}")))
+        print(bold(bad(f'{args.url}')))
+        exit()
+    except Exception:
+        print(bold(bad(bold(red('connection error')))))
+        print(bold(bad(f'{args.url}')))
+        exit()
+
 
     headers = response.headers
     html = response.text
     soup = BeautifulSoup(html, "lxml")
 
     check_headers = [
-        'X-Content-Type-Options',
-        'X-Frame-Options',
-        'X-XSS-Protection',
-        'Strict-Transport-Security',
-        'Content-Security-Policy',
-        'Referrer-Policy',
-        'Feature-Policy'
+        'X-Content-Type-Options',      
+        'X-Frame-Options',             
+        'X-XSS-Protection',            
+        'Strict-Transport-Security',   
+        'Content-Security-Policy',     
+        'Referrer-Policy'              
     ]
 
     cookie_checks = [
-        'Expires',
-        'HttpOnly',
+        'Expires',  
+        'HttpOnly', 
         'Secure',
-        'Path=/',
+        'SameSite'
     ]
 
-    print(run(f"{bold('Request URL')}: {args.url}"))
+    # Verifica código HTTP da response
+    print(run(f"{bold('URL testada')}: {args.url}"))
     if response.status_code == 200:
-        print(good(f"{bold('Response status code')}: {response.status_code}")) 
+        print(good(f"{bold('Response status')}: {response.status_code}")) 
     elif response.status_code in [300,301,302] :
-        print(info(f"{bold('Response status code')}: {response.status_code}")) 
+        print(info(f"{bold('Response status')}: {response.status_code}")) 
     else:
-        print(bad(f"{bold('Response status code')}: {response.status_code}")) 
+        print(bad(f"{bold('Response status')}: {response.status_code}")) 
 
+    # Caso verbose seja true nas flags, mostra a request e response.
     if args.verbose:
         print(info(bold('Request headers:')))
         print(json.dumps(dict(session.headers), indent=2, sort_keys=True))
@@ -86,30 +136,30 @@ def main():
         print(info(bold('Response headers:')))
         print(json.dumps(dict(headers), indent=2, sort_keys=True))
 
-    print(f"\n{run(bold('Checking security headers...'))}")
+    # Começa a checagem dos headers.
+    print(f"\n{run(bold('Checando headers de segurança ...'))}")
+    
     for check_head in check_headers:
         if check_head.lower() in headers:
-            print(good(f'{check_head} found'))
+            print(good(f'{check_head} Encontrado'))
+            validaHeader(check_head, headers)
         else:
-            print(bad(f'{check_head} not found'))
-            # if args.description:
-            #     if check_head in descriptions.keys():
-            #         print(descriptions[check_head])
+            print(bad(f'{check_head} Não Encontrado'))
 
-    print(f"\n{run(bold('Checking cookies...'))}")
-    if 'set-cookie' in headers:
-        cookies = headers['Set-Cookie'].split(',')
-        for cookie in cookies:
-            print(f"{bold('cookie: ')} {cookie}")
-            for cookie_check in cookie_checks:
-                if cookie_check.lower() in cookie.lower():
-                    print(good(f'{cookie_check} found'))
-                else:
-                    print(bad(f'{cookie_check} not found'))
-    else:
-        print(info('not found'))
+    # Realiza a separação dos cookies.
+    cookies = headers['Set-Cookie']
+    pattern = r',(?!\s*\d)' # Regex para não separar quando encontrar uma virgula dentro do expires=...
+    cookies_list = re.split(pattern, cookies)
 
-
+    # Começa a checagem de cookies
+    print(f"\n{run(bold('Checando cookies...'))}")
+    for cookie in cookies_list:
+        print(f"{info(bold('cookie: '))} {cookie}")
+        for cookie_check in cookie_checks:
+            if cookie_check.lower() in cookie.lower():
+                print(" ↳",good(f'{cookie_check} Encontrado'))
+            else:
+                print(" ↳",bad(f'{cookie_check} Não encontrado'))
 
 if __name__ == '__main__':
     main()
